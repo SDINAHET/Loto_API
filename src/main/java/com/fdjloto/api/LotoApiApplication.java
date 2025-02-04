c87c38c64e3b115eb14aff0efb3a3ac8f1846a6c
SDINAHET <stephane.dinahet@gmail.com>
2025-02-04 02:35:59 +0100

    Add OpenAPI documentation and implement MySQL configuration; create login page

--- src/main/java/com/fdjloto/api/LotoApiApplication.java
+++ src/main/java/com/fdjloto/api/LotoApiApplication.java
@@ -2,12 +2,18 @@ package com.fdjloto.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(
+    title = "Loto API",
+    version = "1.0",
+    description = "API pour récupérer et analyser les résultats du loto"
+))
 @SpringBootApplication
 public class LotoApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LotoApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LotoApiApplication.class, args);
+    }
 }
+
